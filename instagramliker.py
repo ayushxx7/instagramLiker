7223f50f3d48137d859c601bfc4ae69a3369edd8
Ayush Mandowara <ayushxx7@gmail.com>
2018-06-04 19:52:21 +0530

    Update instagramliker.py

--- instagramliker.py
+++ instagramliker.py
@@ -1,18 +1,28 @@
 import time
 import pyautogui
 
-#### INSTRAGRAM LIKER 1.0 ####
+#### INSTRAGRAM LIKER 1.1 ####
 
 pyautogui.hotkey('alt', 'tab') #switch to instagram where image is opened
 for z in range(0,100): #specify number of images to scroll(change 100) 
 	time.sleep(0.5) #set processing time according to your pc & internet speed. 
-	if (pyautogui.locateOnScreen('instaCheck.png')) != None: #check image is liked or not
-		pyautogui.doubleClick()
-		# pyautogui.leftClick()
-		# pyautogui.leftClick() # double click to like insta images on website
-		# #pyautogui.press('right') #move to next image
-		time.sleep(0.2) #set processing time according to your pc & internet speed.
-	pyautogui.press('right') #move to next image    	
+	pyautogui.doubleClick()
+	pyautogui.press('right') #move to next image  
+	
+	
+#### INSTRAGRAM LIKER 1.0 ####
+
+#OLD CODE. In instagram images are not unliked by double clicking, so no need to check if image is liked or not!	
+# pyautogui.hotkey('alt', 'tab') #switch to instagram where image is opened
+# for z in range(0,100): #specify number of images to scroll(change 100) 
+# 	time.sleep(0.5) #set processing time according to your pc & internet speed. 
+# 	if (pyautogui.locateOnScreen('instaCheck.png')) != None: #check image is liked or not
+# 		pyautogui.doubleClick()
+# 		# pyautogui.leftClick()
+# 		# pyautogui.leftClick() # double click to like insta images on website
+# 		# #pyautogui.press('right') #move to next image
+# 		time.sleep(0.2) #set processing time according to your pc & internet speed.
+# 	pyautogui.press('right') #move to next image    	
 #basic functionality for now.
 #STEPS > 
 #open instagram with the first image of a page to like
